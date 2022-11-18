78d8fbc22d33150a363bf1de01da1415d1e4a3de
zimoff <pizdecinsane@gmail.com>
2022-11-18 23:54:00 +0600

    added package my.library

---
+++ my/library/Book.java
@@ -0,0 +1,32 @@
+package my.library;
+class Book {
+    private String author;
+    private String name;
+    private int pageNum;
+
+    Book(String author, String name, int pageNum) {
+        this.author = author;
+        this.name = name;
+        this.pageNum = pageNum;
+    }
+
+    String getAuthor() { 
+        return author;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    int getPageNum() { 
+        return pageNum;
+    }
+
+    String getDescription() { 
+        return ("Author: " + author +
+                "\nBook name: " + name + 
+                "\nAmount pages: " + pageNum);
+    }
+
+
+}
