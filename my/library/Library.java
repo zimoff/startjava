75526322fae9bb0c2321c845fa3583b24029eaab
zimoff <pizdecinsane@gmail.com>
2022-12-02 23:10:19 +0300

    renamed methods

--- my/library/Library.java
+++ my/library/Library.java
@@ -3,91 +3,89 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 class Library {
-    private ArrayList<Book> Books = new ArrayList<>();
+    private ArrayList<Book> books = new ArrayList<>();
 
-    //obj
-    void addBook(Book bookObj) {
-        Books.add(bookObj);
+    void addBook(Book book) {
+        books.add(book);
     }
 
-    void deleteBook(Book bookObj) {
-        Books.remove(bookObj);
+    void deleteBook(Book book) {
+        books.remove(book);
     }
 
-    ArrayList<Book> getBooks() {
-        return Books;
+    List<Book> getBooks() {
+        return books;
     }
 
-    void showBooks(List<Book> listOfBooks) {
+
+    List<Book> findBooksByAuthor(String authorName) {
+
+        List<Book> autorsBooks = new ArrayList<>();
+        for(Book book: books) {
+            if(authorName.equals(book.getAuthor()))
+                autorsBooks.add(book);
+        }
+        return autorsBooks;
+    }
+
+    List<Book> WithNameContains(String string) {
+        List<Book> autorsBooks = new ArrayList<>();
+        for(Book book: books) {
+            if(book.getName().contains(string))
+                autorsBooks.add(book);
+        }
+        return autorsBooks;
+    }
+
+    static void showBooks(List<Book> books) {
         System.out.println("-----------");
-        for(Book bk1: listOfBooks) {
-            System.out.println(bk1.getDescription());
+        for(Book book: books) {
+            System.out.println(book.getDescription());
             System.out.println("-----------");
         }
     }
 
-    List<Book> findBooksByAuthor(String name) {
-
-        List<Book> autorsBooks = new ArrayList<>();
-        for(Book bk: Books) {
-            if(name.equals(bk.getAuthor()))
-                autorsBooks.add(bk);
-        }
-        return autorsBooks;
-    }
-
-    List<Book> findBooksByName(String name) {
-        List<Book> autorsBooks = new ArrayList<>();
-        for(Book bk: Books) {
-            if(bk.getName().contains(name))
-                autorsBooks.add(bk);
-        }
-        return autorsBooks;
-    }
-
     public static void main(String[] args) {
 
-        Library lib = new Library();
+        Library library = new Library();
 
         //adding books
-        lib.addBook(new Book("Herbert George Wells","The Time Machine" ,78));
-        lib.addBook(new Book("Herbert George Wells","The War of the Worlds" ,147));
-        lib.addBook(new Book("Jules Verne","The Mysterious Island" ,492));
-        lib.addBook(new Book("Murray Leinster","The Aliens" ,58));
-        lib.addBook(new Book("Edgar Rice Burroughs", "The Gods of Mars", 227));
+        library.addBook(new Book("Herbert George Wells","The Time Machine" ,78));
+        library.addBook(new Book("Herbert George Wells","The War of the Worlds" ,147));
+        library.addBook(new Book("Jules Verne","The Mysterious Island" ,492));
+        library.addBook(new Book("Murray Leinster","The Aliens" ,58));
+        library.addBook(new Book("Edgar Rice Burroughs", "The Gods of Mars", 227));
 
         System.out.println("List of added books: \n");
-        for(Book bk: lib.getBooks()) 
-            System.out.println("|" + lib.getBooks().indexOf(bk) + "| " + bk.getName());
+        for(Book book: library.getBooks()) 
+            System.out.println("|" + library.getBooks().indexOf(book) + "| " + book.getName());
         
         System.out.println(">>>>>>>>>>>>>>>>>>>>");
 
         //finding by author Herbert George Wells
         System.out.println("finding by author: Herbert George Wells");
-        lib.showBooks(lib.findBooksByAuthor("Herbert George Wells"));
+        Library.showBooks(library.findBooksByAuthor("Herbert George Wells"));
 
         System.out.println(">>>>>>>>>>>>>>>>>>>>");
 
         //finding by name 
         System.out.println("\nfinding by name: The War");
-        lib.showBooks(lib.findBooksByName("The War"));
+        Library.showBooks(library.WithNameContains("The War"));
 
         System.out.println(">>>>>>>>>>>>>>>>>>>>");
 
-        //deliting books
+        //deleting books
         System.out.println("\nDeliting books: ");
-        for(Book bk: lib.getBooks()) 
-            System.out.println("|" + lib.getBooks().indexOf(bk) + "| " + bk.getName());
+        for(Book book: library.getBooks()) 
+            System.out.println("|" + library.getBooks().indexOf(book) + "| " + book.getName());
 
-        lib.deleteBook(lib.getBooks().get(0));
-        lib.deleteBook(lib.getBooks().get(0));
+        library.deleteBook(library.getBooks().get(0));
+        library.deleteBook(library.getBooks().get(0));
 
         System.out.println("=========");
 
-        for(Book bk: lib.getBooks()) 
-            System.out.println("|" + lib.getBooks().indexOf(bk) + "| " + bk.getName());
-        System.out.println("|test|test|test|");
-        //lib.testMeth(lib.getBooks());
+        for(Book book: library.getBooks()) 
+            System.out.println("|" + library.getBooks().indexOf(book) + "| " + book.getName());
 
     }
 }
